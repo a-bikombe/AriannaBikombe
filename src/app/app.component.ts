18be577c95d47600ad42866f90f7ee57ada30dfc
Arianna Bikombe <ariannabikombe@gmail.com>
2024-12-04 20:09:28 -0500

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [MatToolbarModule, RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'AriannaBikombe';
+    title = 'Arianna Bikombe\'s Personal Website!';
 }
